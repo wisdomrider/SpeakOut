58240b1001f6b2ddc097421abb35f63cbbdabea2
MacNew <newmac1000@gmail.com>
2019-07-20 14:15:26 +0545

    Android project init

---
+++ android/app/src/main/java/com/org/speakout/HomePageActivity.java
@@ -0,0 +1,14 @@
+package com.org.speakout;
+
+import android.os.Bundle;
+
+import com.wisdomrider.Activities.BaseActivity;
+
+public class HomePageActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ android/app/src/main/java/com/org/speakout/splashscreen/SplashScreen.java
@@ -0,0 +1,15 @@
+package com.org.speakout.splashscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.org.speakout.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
