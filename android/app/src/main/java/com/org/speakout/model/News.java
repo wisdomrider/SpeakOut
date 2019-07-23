b22522cca30bd4164e11057a2ec3ef912433a113
avishek <avishekzone@gmail.com>
2019-07-23 20:24:51 +0545

    web branch created

--- android/app/src/main/java/com/org/speakout/model/News.java
+++
@@ -1,4 +0,0 @@
-package com.org.speakout.model;
-
-public class News {
-}
--- android/app/src/main/java/com/org/speakout/service/MyLocationInterface.java
+++
@@ -1,6 +0,0 @@
-package com.org.speakout.service;
-
-public interface MyLocationInterface {
-
-    void getLocation(double latitude, double longitude);
-}
